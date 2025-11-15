0e6f4bbbe23f0033c053a7e76aa4445fa2e0c0ae
sagarkumarsaini11 <154041506+sagarkumarsaini11@users.noreply.github.com>
2025-11-15 17:26:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const port=process.env.PORT || 5000
 
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://realtime-chat-application-o9o0.onrender.com",
     credentials:true
 }))
 app.use(express.json())
